cc888c792beb3f01006d64bd4bff01188732dd81
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2013-08-17 18:10:10 -0400

    last attempt

--- popup.js
+++ popup.js
@@ -3,10 +3,19 @@ function translateIt() {
     document.getElementById('output').innerHTML = input;
 }
 
+function pop_img() {
+    var img="lost-in-translation.jpg";
+    w=open("",'image','weight=toolbar=no,scrollbars=no,resizable=yes,width=483,height=535');
+    w.document.write("<HTML><BODY onblur=\"window.close();\"><iMG src='"+img+"'>");
+    w.document.write("</BODY></HTML>");
+    w.document.close();
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var translate = document.getElementById('submit');
     translate.addEventListener('click', function() {
         translateIt();
+        pop_img();
     });
 });
