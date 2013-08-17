bca993f747f41d9e704f9411b7389d70514f9f8f
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2013-08-17 13:49:05 -0400

    working js

---
+++ popup.js
@@ -0,0 +1,11 @@
+function translateIt() {
+    var input = document.getElementById('input').value;
+    document.getElementById('output').innerHTML = input;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var translate = document.getElementById('submit');
+    translate.addEventListener('click', function() {
+        translateIt();
+    });
+});
