0a1ac94ec3765bbd16f30b0e08aae54067bf653c
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2013-08-17 15:10:28 -0400

    color

--- popup.js
+++ popup.js
@@ -1,24 +1,12 @@
-var goTranslate = {
-
-    searchOnGoogle_: 'http://translate.google.com/#fr/en/this%20is%20my%20home%0Awhere%20are%20you%20living',
-
-    requestTranslate: function() {
-        var req = new XMLHttpRequest();
-        req.open("GET", this.searchOnGoogle_, true);
-        req.onload = this.showTranslation_.bind(this);
-        req.send(null);
-    },
-
-    showTranslation_: function (e) {
-        var text = e.target.responseHTML.querySelectorAll('span');
-        document.getElementById('output').innerHTML = text;
-    }
-};
+function translateIt() {
+    var input = document.getElementById('input').value;
+    document.getElementById('output').innerHTML = input;
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {
     var translate = document.getElementById('submit');
     translate.addEventListener('click', function() {
-        goTranslate.requestTranslate();
+        translateIt();
     });
 });
